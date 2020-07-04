3c05774dfe11eede2e0ac8e616725120ba74bbe5
zaidbhat1234 <33365681+zaidbhat1234@users.noreply.github.com>
2020-07-04 21:26:49 +0530

    Update Distribute_d.cpp

--- Distribute_d.cpp
+++ Distribute_d.cpp
@@ -37,14 +37,11 @@ using namespace std;
 using namespace CryptoPP;
 
 void Distribute_d(mpz_class a, int n, mpz_class *p1,mpz_class p_phi){
-    
     mpz_class p;
-    
     mpz_class temp;
     mpz_class temp_d;
     mpz_class gcd;
     mpz_class x,y;
-    
     // mpz_t r4;
     //gmp_randstate_t t3;
     //gmp_randinit_default (t3);
@@ -53,42 +50,28 @@ void Distribute_d(mpz_class a, int n, mpz_class *p1,mpz_class p_phi){
     //mpz_init(temp1);
     int i;
     for (i=0; i<n-1; i++) {
-        
         temp= rand();
         temp %= ((p_phi/2)-1);
         temp_d = 2*temp+1;
         gcd = gcdExtended(temp_d, p_phi, &x, &y);
-        
         while (gcd!=1) {
-            
             temp= rand();
             temp %= ((p_phi/2)-1);
             temp_d = 2*temp+1;
             gcd = gcdExtended(temp_d, p_phi, &x, &y);
-            
             // cout<<"di is "<<temp_d<<'\n';
-            
         }
-        
-        
         //Generate d's such that their gcd with p-1 is 1. even if we do it by hit and trial for n-1 d's we will still not be sure if the last subtracted d will be co prime to p-1
-        
-        
         /*
          while(temp<0||temp>a/n){
          temp = rand();
          gcd = gcdExtended(temp, p_phi, &x, &y);
          }
          */
-        
-        
-        
-        
         // mpz_set_ui(temp1,temp);
         //mpz_mul(r4,r4,temp1);
         //char *s = mpz_get_str(NULL,10,r4);
         //string str = s;
-        
         mpz_class p = temp_d;
         p1[i]=p;
         //cout<<"di is "<<p<<'\n';
@@ -96,25 +79,20 @@ void Distribute_d(mpz_class a, int n, mpz_class *p1,mpz_class p_phi){
         
     }
     mpz_class gcd1;
-    
     gcd1 = gcdExtended(a, p_phi, &x, &y);
     //cout<<"GCD 1 is "<<gcd1<<'\n';
     i = 3;
     gcd = gcdExtended(p1[i], p_phi, &x, &y);
     //cout<<"GCD i is "<<gcd<<'\n';
-    
     //BELOW LOOP WORKS WHEN N_D IS ODD , FOR EVEN , THE SUM OF EVEN NUMBER OF N_D-2 IS EVEN , BECAUSE EACH DI IS INDIVIDUALLY ODD , SUM OF EVEN NUMBER OF ODD NUMBERS IS EVEN , SO IT DOESNT WORK WHEN N_D IS EVEN, in n_d as even, the n-1 shares are odd and thus nth share is even to make the final d which is odd, so it wont work for n_d as even but only when n_d is odd
     while (gcd1!=1) {
         gcd =rand();
         while (gcd!=1) {
-            
             temp= rand();
             temp %= ((p_phi/2)-1);
             temp_d = 2*temp+1;
             gcd = gcdExtended(temp_d, p_phi, &x, &y);
-            
             //cout<<"di is "<<temp_d<<'\n';
-            
         }
         //cout<<"P is "<<a<<'\n';
         a=a+p1[i];
@@ -128,11 +106,6 @@ void Distribute_d(mpz_class a, int n, mpz_class *p1,mpz_class p_phi){
         // cout<<"GCD 1 is "<<gcd1<<'\n';
         // cout<<"GCD i is "<<gcd1<<'\n';
     }
-    
-    
-    
-    
-    
     p1[n-1]=a;
     
     
